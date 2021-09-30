84e3a2021574e84dadeeff81e8ecc24e4c3b528b
Josh Fairhead <hello@joshafairhead.com>
2021-09-30 16:36:45 +0200

    initial commit

---
+++ src/pages/Index1.vue
@@ -0,0 +1,33 @@
+<template>
+  <Layout>
+
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <g-image alt="me" src="~/assets/MeThumbs.jpg" width="170" />
+
+    <h1>Welcome to my digital garden</h1>
+
+    <p>
+      Simply put the intention of this site is to bring my Obsidian notes alive!
+    </p>
+
+    <p class="home-links">
+      <a href="https://joshafairhead.com" target="_blank" rel="noopener">Website</a>
+      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+    </p>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Home'
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
